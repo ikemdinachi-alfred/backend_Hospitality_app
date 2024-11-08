088e54c852e4cc3ede8ec4a2f4e224f175203f14
ikemdinachi-alfred <ikemdinachialfred@gmail.com>
2024-11-08 10:01:53 +0100

    user Services

--- hospitality_management_application/src/main/java/com/alfredTech/hospitality_management_application/controllers/UserController.java
+++ hospitality_management_application/src/main/java/com/alfredTech/hospitality_management_application/controllers/UserController.java
@@ -1,4 +1,24 @@
 package com.alfredTech.hospitality_management_application.controllers;
+import com.alfredTech.hospitality_management_application.dtos.reponse.Response;
+import com.alfredTech.hospitality_management_application.services.interfac.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
+@RequestMapping("/users")
 public class UserController {
+    @Autowired
+    private UserService userService;
+
+    @GetMapping("/all")
+    @PreAuthorize("hasAuthority('ADMIN')")
+    public ResponseEntity<Response> getAllUsers() {
+        Response response = userService.getAllUsers();
+        return ResponseEntity.status(response.getStatusCode()).body(response);
+    }
+
 }
--- hospitality_management_application/src/main/java/com/alfredTech/hospitality_management_application/services/impl/UserServiceImpl.java
+++ hospitality_management_application/src/main/java/com/alfredTech/hospitality_management_application/services/impl/UserServiceImpl.java
@@ -1,45 +1,189 @@
 package com.alfredTech.hospitality_management_application.services.impl;
 
 import com.alfredTech.hospitality_management_application.data.models.User;
+import com.alfredTech.hospitality_management_application.data.repositories.UserRepository;
 import com.alfredTech.hospitality_management_application.dtos.reponse.Response;
 import com.alfredTech.hospitality_management_application.dtos.requests.LoginRequest;
+import com.alfredTech.hospitality_management_application.dtos.requests.UserDTO;
+import com.alfredTech.hospitality_management_application.exception.OurException;
 import com.alfredTech.hospitality_management_application.services.interfac.UserService;
+import com.alfredTech.hospitality_management_application.utils.JWTUtils;
+import com.alfredTech.hospitality_management_application.utils.Utils;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
+@RequiredArgsConstructor
 public class UserServiceImpl implements UserService {
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+    private final AuthenticationManager authenticationManager;
+    private JWTUtils jwtUtils;
+
+
+
     @Override
     public Response registerUser(User user) {
-        return null;
+        Response response = new Response();
+
+        try {
+            if (user.getRole()==null || user.getRole().isBlank()) {
+                user.setRole("USER");
+            }
+            if (userRepository.existsByEmail(user.getEmail())){
+                throw new OurException(user.getEmail()+ " is already registered");
+            }
+            user.setPassword(passwordEncoder.encode(user.getPassword()));
+            User savedUser = userRepository.save(user);
+            UserDTO userDTO = Utils.mapUserModelToUserDTO(savedUser);
+            response.setStatusCode(200);
+            response.setUser(userDTO);
+        } catch (OurException e) {
+            response.setStatusCode(400);
+           throw new OurException(e.getMessage());
+        }catch (Exception e) {
+            response.setStatusCode(500);
+            response.setMessage("Error Occurred During User Registration "+e.getMessage());
+        }
+        return response;
     }
 
     @Override
     public Response loginUser(LoginRequest loginRequest) {
-        return null;
+        Response response = new Response();
+        try {
+            authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(loginRequest.getEmail(), loginRequest.getPassword()));
+            var user = userRepository.findByEmail(loginRequest.getEmail()).orElseThrow(()-> new OurException("User Not Found"));
+
+            var token = jwtUtils.generateToken(user);
+            response.setStatusCode(200);
+            response.setToken(token);
+            response.setRole(user.getRole());
+            response.setExpirationTime("7 Days");
+            response.setMessage("Successfully logged in");
+        }catch (OurException e) {
+            response.setStatusCode(404);
+            response.setMessage(e.getMessage());
+        }catch (Exception e) {
+            response.setStatusCode(500);
+            response.setMessage("Error Occurred During User Login "+e.getMessage());
+        }
+
+        return response;
     }
 
     @Override
     public Response getAllUsers() {
+        Response response = new Response();
+
+        try {
+            List<User> userList = userRepository.findAll();
+            List<UserDTO> userDTOList = Utils.mapUseListModelToUserListDTO(userList);
+            response.setStatusCode(200);
+            response.setMessage("successful");
+            response.setUserList(userDTOList);
+
+        } catch (Exception e) {
+            response.setStatusCode(500);
+            response.setMessage("Error getting all users " + e.getMessage());
+        }
         return null;
     }
 
     @Override
     public Response getUserBookingHistory(String userId) {
-        return null;
+        Response response = new Response();
+
+
+        try {
+            User user = userRepository.findById(Long.valueOf(userId)).orElseThrow(() -> new OurException("User Not Found"));
+            UserDTO userDTO = Utils.mapUserModelToUserDTOPlusUserBookingAndRoom(user);
+            response.setStatusCode(200);
+            response.setMessage("successful");
+            response.setUser(userDTO);
+
+        } catch (OurException e) {
+            response.setStatusCode(404);
+            response.setMessage(e.getMessage());
+
+        } catch (Exception e) {
+
+            response.setStatusCode(500);
+            response.setMessage("Error getting all users " + e.getMessage());
+        }
+        return response;
     }
 
     @Override
     public Response deleteUser(String userId) {
-        return null;
+        Response response = new Response();
+
+        try {
+            userRepository.findById(Long.valueOf(userId)).orElseThrow(() -> new OurException("User Not Found"));
+            userRepository.deleteById(Long.valueOf(userId));
+            response.setStatusCode(200);
+            response.setMessage("successful");
+
+        } catch (OurException e) {
+            response.setStatusCode(404);
+            response.setMessage(e.getMessage());
+
+        } catch (Exception e) {
+
+            response.setStatusCode(500);
+            response.setMessage("Error getting all users " + e.getMessage());
+        }
+        return response;
     }
 
     @Override
     public Response getUserById(String userId) {
-        return null;
+        Response response = new Response();
+
+        try {
+            User user = userRepository.findById(Long.valueOf(userId)).orElseThrow(() -> new OurException("User Not Found"));
+            UserDTO userDTO = Utils.mapUserModelToUserDTO(user);
+            response.setStatusCode(200);
+            response.setMessage("successful");
+            response.setUser(userDTO);
+
+        } catch (OurException e) {
+            response.setStatusCode(404);
+            response.setMessage(e.getMessage());
+
+        } catch (Exception e) {
+
+            response.setStatusCode(500);
+            response.setMessage("Error getting all users " + e.getMessage());
+        }
+        return response;
     }
 
     @Override
     public Response getMyInfo(String email) {
-        return null;
+        Response response = new Response();
+
+        try {
+            User user = userRepository.findByEmail(email).orElseThrow(() -> new OurException("User Not Found"));
+            UserDTO userDTO = Utils.mapUserModelToUserDTO(user);
+            response.setStatusCode(200);
+            response.setMessage("successful");
+            response.setUser(userDTO);
+
+        } catch (OurException e) {
+            response.setStatusCode(404);
+            response.setMessage(e.getMessage());
+
+        } catch (Exception e) {
+
+            response.setStatusCode(500);
+            response.setMessage("Error getting all users " + e.getMessage());
+        }
+        return response;
     }
 }
